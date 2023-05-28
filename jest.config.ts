672845257f1736199d3f66aed63bd4663d6e74d1
Aleksandr Kononov <akellastoopi@gmail.com>
2023-05-28 23:05:23 +0000

    remove tests from ci/cd temp

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,11 @@
+import { pathsToModuleNameMapper } from 'ts-jest'
 import {compilerOptions} from './tsconfig.json';
 
 export default {
 	preset: "ts-jest",
 	testEnvironment: "jsdom",
-  modulePaths: [compilerOptions.baseUrl],
-  moduleNameMapper: {
-	"^@/(.*)$": "<rootDir>/src/$1",
-	"^@components/(.*)$": "<rootDir>/src/components/$1",
-	"^@utils/(.*)$": "<rootDir>/src/utils/$1",
-	"^@pages/(.*)$": "<rootDir>/src/pages/$1",
-	"^@redux/(.*)$": "<rootDir>/src/redux/$1",
-	"^@assets/(.*)$": "<rootDir>/src/assets/$1",
-	"^@sass/(.*)$": "<rootDir>/src/sass/$1",
-	"^@hooks/(.*)$": "<rootDir>/src/redux/hooks/$1",
-	"^@styles/(.*)$": "<rootDir>/src/styles/$1",
-	"^@locales/(.*)$": "<rootDir>/src/locales/$1"
-  },
+    modulePaths: [compilerOptions.baseUrl],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 	setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
 	testPathIgnorePatterns: [
 		"/node_modules/",
