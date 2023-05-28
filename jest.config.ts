4d1c726e08997365a89071f7e51573f0a3a61946
Flowerinno <akellastoopi@gmail.com>
2023-05-28 15:53:46 +0300

    configure jest deps

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,16 @@ export default {
 	preset: "ts-jest",
 	testEnvironment: "jsdom",
 	moduleNameMapper: {
-		"^src/(.*)$": "<rootDir>/src/$1",
+		"^@/(.*)$": "<rootDir>/src/$1",
+		"^@components/(.*)$": "<rootDir>/src/components/$1",
+		"^@pages/(.*)$": "<rootDir>/src/pages/$1",
+		"^@utils/(.*)$": "<rootDir>/src/utils/$1",
+		"^@styles": "<rootDir>/src/styles/global.scss",
+		"^@sass/(.*)$": "<rootDir>/src/sass/$1",
+		"^@redux/(.*)$": "<rootDir>/src/redux/$1",
+		"^@hooks/(.*)$": "<rootDir>/src/redux/hooks/$1",
+		"^@layout/(.*)$": "<rootDir>/src/components/layout/$1",
+		"^@locales/(.*)$": "<rootDir>/src/locales/$1",
 	},
 	setupFilesAfterEnv: ["<rootDir>/src/__test__/config/importJestDOM.ts"],
 };
